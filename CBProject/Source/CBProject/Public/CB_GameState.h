1097ecfc4e97179035d95dc1252314bc2144bdea
Mrsangmin <sangmin0280@naver.com>
2025-04-14 11:16:00 +0900

    게임 로직 및 폴더 생성

---
+++ CBProject/Source/CBProject/Public/CB_GameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "CB_GameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CBPROJECT_API ACB_GameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ CBProject/Source/CBProject/Public/CB_PlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "CB_PlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CBPROJECT_API ACB_PlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
