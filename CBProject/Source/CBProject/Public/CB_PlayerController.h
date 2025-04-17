1e0a6c6b9fc6c7fd74ccd8df591f3ba8e52f1f0f
jmin0922 <staring0922@gmail.com>
2025-04-17 12:03:47 +0900

    IMC

--- CBProject/Source/CBProject/Public/CB_PlayerController.h
+++ CBProject/Source/CBProject/Public/CB_PlayerController.h
@@ -6,12 +6,39 @@
 #include "GameFramework/PlayerController.h"
 #include "CB_PlayerController.generated.h"
 
+class UInputMappingContext; // IMC 관련 전방 선언
+class UInputAction; // IA 관련 전방 선언
+
 UCLASS()
 class CBPROJECT_API ACB_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
 public:
+	
+	ACB_PlayerController();
 	virtual void BeginPlay() override;
 	void SetInputEnabled(bool bEnable);
+
+	// 에디터에서 세팅할 IMC
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputMappingContext* InputMappingContext;
+	
+	// IA_Move를 지정할 변수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* MoveAction;
+	
+	// IA_Jump를 지정할 변수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* JumpAction;
+	
+	// IA_Dash를 지정할 변수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* DashAction;
+	
+	// IA_Crouch를 지정할 변수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* CrouchAction;
+
+
 };
